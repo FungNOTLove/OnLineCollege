8866b5f975644b89176b7204e01235e2d1748f30
fungnotl <fungnotl@gmail.com>
2019-11-18 15:49:18 +0800

    date11.17

---
+++ 算法/Algorithm/src/com/DataChecker.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class DataChecker{
+
+  static int[] generateRandomArray(){
+
+    Random r = new Random();
+    int[] arr = new int[10000];
+    for (int i = 0;i < arr.length; i++) {
+      arr[i] = r.nextInt(10000);
+    }
+
+    return arr;
+
+  }
+
+  static void check(){
+    boolean same = true;
+    for (int times = 0; times < 1000; times++) {
+      int[] arr = generateRandomArray();
+      int[] arr2 = new int[arr.length];
+      System.arraycopy(arr, 0, arr2, 0, arr.length);
+
+      Arrays.sort(arr);
+
+
+      BubbleSort.sort(arr2);
+
+      for (int i = 0; i < arr2.length; i++) {
+        if (arr[i] != arr2[i]) {
+          same = false;
+        }
+      }
+    }
+      System.out.println(same == true ? "right" : "wrong");
+
+  }
+
+
+  public static void main(String[] args){
+    check();
+  }
+}
